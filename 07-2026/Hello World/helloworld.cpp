6332f3cf9a16f0a6ee5b396ed910e327766b0fa2
unknown <marssalnascimento@gmail.com>
2026-07-13 14:06:38 -0300

    Corrige pasta 07-2026 para pasta normal

---
+++ 07-2026/Hello World/helloworld.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main(){
+
+    //Isso é um comentário
+
+    /*
+        E isso
+        é um comentário
+        de várias linhas
+    */
+
+    std::cout << "Hello, World!" << '\n';
+    std::cout << "Esse é o meu primeiro programa em C++!" << std::endl;
+    return 0;
+}
